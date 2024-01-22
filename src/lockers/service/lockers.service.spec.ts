99cef375cdd7c0634dc827b907602aa07f001c87
Liès TIGUERCHA <lies.tiguercha@decathlon.com>
2024-01-22 11:31:15 +0100

    Subscribe to RFID topic

--- src/lockers/service/lockers.service.spec.ts
+++ src/lockers/service/lockers.service.spec.ts
@@ -1,79 +1,78 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { LockersService } from './lockers.service';
-import { LockerDto } from '../dto/locker.dto';
-import { LockerEntity } from '../entity/locker.entity';
-import { getRepositoryToken } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import {Test, TestingModule} from '@nestjs/testing';
+import {LockersService} from './lockers.service';
+import {LockerDto} from '../dto/locker.dto';
+import {LockerEntity} from '../entity/locker.entity';
+import {getRepositoryToken} from '@nestjs/typeorm';
+import {Repository} from 'typeorm';
 
-// Créez une classe mock pour le repository
 class LockerRepositoryMock {
-  find(): Promise<LockerEntity[]> {
-    return Promise.resolve([]);
-  }
+    find(): Promise<LockerEntity[]> {
+        return Promise.resolve([]);
+    }
 
-  findOne(id: number): Promise<LockerEntity | undefined> {
-    return Promise.resolve(undefined);
-  }
+    findOne(id: number): Promise<LockerEntity | undefined> {
+        return Promise.resolve(undefined);
+    }
 
-  save(locker: LockerDto): Promise<LockerDto> {
-    return Promise.resolve(locker);
-  }
+    save(locker: LockerDto): Promise<LockerDto> {
+        return Promise.resolve(locker);
+    }
 }
 
 describe('LockersService', () => {
-  let service: LockersService;
-  let repository: Repository<LockerEntity>;
+    let service: LockersService;
+    let repository: Repository<LockerEntity>;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        LockersService,
-        {
-          provide: getRepositoryToken(LockerEntity),
-          useClass: LockerRepositoryMock, // Utilisez la classe mock pour le repository
-        },
-      ],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                LockersService,
+                {
+                    provide: getRepositoryToken(LockerEntity),
+                    useClass: LockerRepositoryMock,
+                },
+            ],
+        }).compile();
 
-    service = module.get<LockersService>(LockersService);
-    repository = module.get<Repository<LockerEntity>>(
-      getRepositoryToken(LockerEntity),
-    );
-  });
-
-  describe('findAll', () => {
-    it('should return an array of lockers', async () => {
-      jest
-        .spyOn(repository, 'find')
-        .mockImplementation(() => Promise.resolve([]));
-
-      const result: LockerDto[] = await service.findAll();
-      expect(result).toEqual([]);
+        service = module.get<LockersService>(LockersService);
+        repository = module.get<Repository<LockerEntity>>(
+            getRepositoryToken(LockerEntity),
+        );
     });
-  });
 
-  describe('findOne', () => {
-    it('should return a single locker by ID', async () => {
-      const lockerId = 1;
-      const mockLocker: LockerEntity = { id: lockerId, id_objet: 2 };
-      jest
-        .spyOn(repository, 'findOne')
-        .mockImplementation(() => Promise.resolve(mockLocker));
+    describe('findAll', () => {
+        it('should return an array of lockers', async () => {
+            jest
+                .spyOn(repository, 'find')
+                .mockImplementation(() => Promise.resolve([]));
 
-      const result: LockerDto = await service.findOne(lockerId);
-      expect(result).toEqual(mockLocker);
+            const result: LockerDto[] = await service.findAll();
+            expect(result).toEqual([]);
+        });
     });
-  });
 
-  describe('createLocker', () => {
-    it('should create a new locker', async () => {
-      const newLockerData: LockerDto = { id: 1, id_objet: 2 };
-      jest
-        .spyOn(repository, 'save')
-        .mockImplementation(() => Promise.resolve(newLockerData));
+    describe('findOne', () => {
+        it('should return a single locker by ID', async () => {
+            const lockerId = 1;
+            const mockLocker: LockerEntity = {id: lockerId, id_objet: 2};
+            jest
+                .spyOn(repository, 'findOne')
+                .mockImplementation(() => Promise.resolve(mockLocker));
 
-      const result: LockerDto = await service.createLocker(newLockerData);
-      expect(result).toEqual(newLockerData);
+            const result: LockerDto = await service.findOne(lockerId);
+            expect(result).toEqual(mockLocker);
+        });
     });
-  });
+
+    describe('createLocker', () => {
+        it('should create a new locker', async () => {
+            const newLockerData: LockerDto = {id: 1, id_objet: 2};
+            jest
+                .spyOn(repository, 'save')
+                .mockImplementation(() => Promise.resolve(newLockerData));
+
+            const result: LockerDto = await service.createLocker(newLockerData);
+            expect(result).toEqual(newLockerData);
+        });
+    });
 });
--- src/mqtt/service/mqtt.service.ts
+++ src/mqtt/service/mqtt.service.ts
@@ -1,38 +1,39 @@
 // mqtt.service.ts
-import { Injectable } from '@nestjs/common';
-import mqtt, { IClientOptions } from 'mqtt';
+import {Injectable} from '@nestjs/common';
+import mqtt, {IClientOptions} from 'mqtt';
 
 @Injectable()
 export class MqttService {
-  private client: mqtt.MqttClient;
+    private client: mqtt.MqttClient;
 
-  constructor() {
-    const options: IClientOptions = {
-      host: '9badbe94c25a4774be743345e8145009.s2.eu.hivemq.cloud',
-      port: 8883,
-      protocol: 'mqtts',
-      username: 'admin',
-      password: 'Adminjunia59',
-    };
+    constructor() {
+        const options: IClientOptions = {
+            host: '9badbe94c25a4774be743345e8145009.s2.eu.hivemq.cloud',
+            port: 8883,
+            protocol: 'mqtts',
+            username: 'admin',
+            password: 'Adminjunia59',
+        };
 
-    this.client = mqtt.connect(options);
+        this.client = mqtt.connect(options);
 
-    this.client.on('connect', () => {
-      console.log('Connected');
-    });
+        this.client.on('connect', () => {
+            console.log('Connected');
+        });
 
-    this.client.on('error', (error) => {
-      console.log(error);
-    });
+        this.client.on('error', (error) => {
+            console.log(error);
+        });
 
-    this.client.on('message', (topic, message) => {
-      console.log('Received message:', topic, message.toString());
-    });
+        this.client.on('message', (topic, message) => {
+            console.log('Received message:', topic, message.toString());
+        });
 
-    this.client.subscribe('locker/open');
-  }
+        this.client.subscribe('locker/open');
+        this.client.subscribe('rfid/read');
+    }
 
-  publishMessage(topic: string, message: string): void {
-    this.client.publish(topic, message);
-  }
+    publishMessage(topic: string, message: string): void {
+        this.client.publish(topic, message);
+    }
 }
