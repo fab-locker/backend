cc6186cae56b54f20cdc3726acb08f0df572110b
Liès TIGUERCHA <lies.tiguercha@decathlon.com>
2024-02-20 17:20:03 +0100

    Register endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,22 @@
-import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
+import { CreateUsersDto } from '../users/dto/createUsers.dto';
+import { UsersService } from '../users/service/users.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private usersService: UsersService) {
   }
 
   @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Request() req) {
     return await this.authService.login(req.user);
+  }
+
+  @Post('register')
+  async register(@Body() createUserDto: CreateUsersDto) {
+    return await this.usersService.create(createUserDto);
   }
 }
