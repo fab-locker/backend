3a1a7092612beeebc7aff2bbaee6b92125a4b4a1
Liès TIGUERCHA <lies.tiguercha@decathlon.com>
2024-05-07 17:28:43 +0200

    Ajout de décorateurs pour le swagger :memo:

--- src/items/controller/items.controller.ts
+++ src/items/controller/items.controller.ts
@@ -3,7 +3,7 @@ import { BaseItemDto } from '../dto/base-items.dto';
 import { ItemsService } from '../service/items.service';
 import { UpdateItemDto } from '../dto/update-item.dto';
 import { CreateItemDto } from '../dto/create-item.dto';
-import { ApiOkResponse, ApiOperation, ApiQuery, ApiTags } from '@nestjs/swagger';
+import { ApiOkResponse, ApiOperation, ApiQuery, ApiTags, ApiBody } from '@nestjs/swagger';
 import { Roles } from '../../auth/roles/roles.decorator';
 import { JwtGuard } from '../../auth/guards/jwt.guard';
 import { RoleGuard } from '../../auth/role/role.guard';
@@ -15,7 +15,6 @@ export class ItemsController {
   constructor(private itemService: ItemsService) {
   }
 
-
   @ApiOperation({ summary: 'Get item(s) (with optional filters)' })
   @ApiOkResponse({ description: 'Success', type: [BaseItemDto] })
   @Roles(Role.User)
@@ -25,22 +24,22 @@ export class ItemsController {
     return this.itemService.getItems(item);
   }
 
-
   @ApiOperation({ summary: 'Create item (with optional filters)' })
   @ApiOkResponse({ description: 'Success', type: [CreateItemDto] })
   @Roles(Role.Admin)
   @UseGuards(JwtGuard, RoleGuard)
+  @ApiBody({ type: CreateItemDto })
   @Post()
   async createItem(@Body() item: CreateItemDto): Promise<CreateItemDto | null | ConflictException> {
     return this.itemService.createItem(item);
   }
-
 
   @ApiOperation({ summary: 'Update item (with optional filters)' })
   @ApiOkResponse({ description: 'Success', type: [UpdateItemDto] })
   @ApiQuery({ name: 'id', type: 'number', required: true })
   @Roles(Role.Admin)
   @UseGuards(JwtGuard, RoleGuard)
+  @ApiBody({ type: UpdateItemDto })
   @Patch(':id')
   async updateItem(@Param('id') id: number, @Body() newItem: Partial<UpdateItemDto>): Promise<UpdateItemDto | null> {
     return this.itemService.updateItem(id, newItem);
@@ -55,5 +54,4 @@ export class ItemsController {
   async deleteItem(@Param('id') id: number): Promise<{ statusCode: number; message: string }> {
     return this.itemService.deleteItem(id);
   }
-
 }
